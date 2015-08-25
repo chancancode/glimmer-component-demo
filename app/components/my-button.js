ba92e541a2e7d48ac4119be1f21944102aeca320
Godfrey Chan <godfreykfc@gmail.com>
2015-08-25 13:30:25 -0700

    Unwrap my-button

--- app/components/my-button.js
+++ app/components/my-button.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'button',
+  classNames: ['btn', 'btn-lg', 'btn-primary', 'btn-block'],
+  ariaRole: 'button',
+  attributeBindings: ['type'],
+  type: 'button'
 });
